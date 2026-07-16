2b35b2f0750f69834a0d4e95f22ed2d3ae2fbdf2
TeemoCaption <eyan0816@gmail.com>
2026-07-16 17:06:44 +0800

    整理模板示範元件與更新鎖定檔依賴資訊

---
+++ src/components/Temp03.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>第三小節-響應式狀態</h1>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+// 宣告響應式狀態(如果資料屬於同一個使用者物件)
+const user = reactive({
+    name: 'Alice',
+    age: 20,
+    email: 'alice@example.com',
+    isLoggedIn: true
+})
+
+</script>
+
+<style scoped></style>

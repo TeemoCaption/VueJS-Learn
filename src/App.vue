06a4b1a2042d9b1fd5d8cda41fdef34bd482ac26
楊承諺 <M11323006@live.yuntech.edu.tw>
2026-07-27 01:28:33 +0800

    提交第06單元學習內容

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup></script>
+<script setup>
+import MyButton from './components/MyButton.vue'
+
+// 事件也會透傳
+function clickButton() {
+  console.log('按鈕被點擊')
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <!-- 透傳屬性：根本沒有寫 class 和 id，但 Vue 自己幫我加上去了。幾乎所有一般 HTML 屬性都可以透傳
+；class 和 style 也會透傳，不是覆蓋，而是合併! -->
+  <MyButton @click="clickButton" class="red" id="loginBtn" title="登入" style="color:red" />
+
 </template>
 
 <style scoped></style>
